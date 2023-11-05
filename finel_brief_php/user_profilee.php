0954a1d65c130efbedaaabe400e6ec8211c6b905
reem <144600489+ReemHasanA@users.noreply.github.com>
2023-11-05 20:30:08 +0300

    Add files via upload

--- finel_brief_php/user_profilee.php
+++ finel_brief_php/user_profilee.php
@@ -56,8 +56,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           body {
             font-family: Arial, sans-serif;
             background-color:#ede4d6;
+            
         }
-        
+        main{
+            display: flex;
+            justify-content:space-around;
+        }
+        ul{
+            background-color: #32745c ;
+            min-width: 300px;
+            margin: 0 auto;
+            list-style-type: none;
+            padding: 20px;
+            overflow-y: auto;
+            height: 300px;
+        }
+        li{
+            border-bottom: 1px  solid white;
+        }
         h1 {
             text-align: center;
 
@@ -67,8 +83,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             width: 300px;
             margin: 0 auto;
             padding: 20px;
-            border: 1px solid #ccc;
-            background-color: #32745c ;        }
+            /* border: 1px solid #ccc; */
+            background-color: #32745c ;
+            height:300px;     
+        }
         
         label {
             display: block;
@@ -86,7 +104,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             border-radius: 5px;
         }
         
-        input[type="submit"] {
+        input[type="submit"],button {
             background-color: #007BFF;
             color: #fff;
             padding: 10px 20px;
@@ -95,13 +113,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             cursor: pointer;
         }
         
-        input[type="submit"]:hover {
+        input[type="submit"]:hover ,button:hover{
             background-color: #0056b3;
         }
-    </style>
+        a{
+            color:white;
+            text-decoration: none;
+        }
+        </style>
 </head>
 <body>
-    <h1>Edit Your Profile</h1>
+    <h1>Welcome <?php echo $userData['name']; ?></h1>
+    
+    <main>
+    <section>
+        <h2>your events</h2>
+        <ul>
+        <?php
+        $conn = mysqli_connect('localhost','root','','test333');
+        $id=
+        $sql = "SELECT * FROM users INNER JOIN history ON users.id = history.id JOIN events ON history.events_id=events.events_id WHERE users.email='$userEmail'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+    
+            while ($row=$result->fetch_assoc()) {
+                echo '<li>';
+                echo '<a href="enter_event.php?id=' . $row['events_id'] . '" >
+                <h3>' . $row['title'] . '</h3>'.
+                '<p>Date: ' . $row['created_at'] . '</p>
+                </a>';
+                
+                echo '</li>';
+                
+            }
+        }
+        ?>
+        </ul>
+    </section>
+    <div>
+        <h2>Edit Your Profile</h2>
     <form method="POST">
         <label for="name">Name:</label>
         <input type="text" name="name" value="<?php echo $userData['name']; ?>" required><br>
@@ -113,7 +163,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <input type="text" name="password" value="<?php echo $userData['password']; ?>" required><br>
 
         <input type="submit" value="Update">
-        <a href="events.php">BACK TO HOME</a>
+        <a href="events.php" style='color:white'>BACK TO HOME</a>
     </form>
+    </div>
+    </main>
 </body>
 </html>
